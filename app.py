99ce0c0056f8b4c4d32436bea674b2614aa27962
RajaSab-tech <rajasab4770@gmail.com>
2026-03-13 21:30:37 +0530

    Added ML project files

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model = pickle.load(open("model.pkl","rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict",methods=["POST"])
+def predict():
+
+    hours = float(request.form["hours"])
+    attendance = float(request.form["attendance"])
+    marks = float(request.form["marks"])
+
+    prediction = model.predict([[hours,attendance,marks]])
+
+    if prediction[0] == 1:
+        result = "Student will PASS"
+    else:
+        result = "Student will FAIL"
+
+    return render_template("index.html",prediction_text=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
